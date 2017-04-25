fea0bc594501631dddee78b67893fd6b62e98243
meryldakin <meryl.k.dakin@gmail.com>
2017-04-25 10:32:04 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function iterativeLog(array) {
+  array.forEach(function(element, index) {
+    console.log( `${index}: ${element}`);
+  });
+}
+
+
+function iterate(callback) {
+  var array = [];
+  callback(array);
+  return array;
+}
+
+function doToArray(array, callback){
+  array.forEach(callback);
+  return array;
+}
